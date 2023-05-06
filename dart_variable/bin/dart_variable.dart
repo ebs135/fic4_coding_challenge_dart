29892237cf81c083736c45d35ff1deb3649c4563
Eko B Santoso <ebs135@gmail.com>
2023-05-06 14:56:35 +0700

    Latihan DART variabel inisial commit

---
+++ dart_variable/bin/dart_variable.dart
@@ -0,0 +1,9 @@
+import 'package:dart_variable/dart_variable.dart' as dart_variable;
+
+void main(List<String> arguments) {
+  int panjang = 5;
+  int lebar = 2;
+  int luas = panjang * lebar;
+
+  print('Panjang: $panjang, Lebar: $lebar, Luas = $panjang x $lebar = $luas');
+}
